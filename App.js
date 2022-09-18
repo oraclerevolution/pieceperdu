17ed0f08c9e6aac77c3d7b569446300563d9f821
oracle2_dev <mac.n.drix@gmail.com>
2022-09-18 08:28:46 +0000

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,72 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+//imported packages
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
+//imported pages
+import Login from './screens/Login';
+import Register from './screens/Register';
+import Declaration from './screens/Tabs/Declaration';
+import Home from './screens/Tabs/Home';
+import Steppers from './screens/Steppers';
+
+//variables declaration
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function App() {
+
+  const MyTabs = () => {
+    return (
+      <Tab.Navigator
+        initialRouteName="Declaration"
+        activeColor="#fff"
+        labelStyle={{ fontSize: 12}}
+        style={{ backgroundColor: 'tomato' }}
+        barStyle={{backgroundColor:"#573178"}}
+        screenOptions={{ headerShown: false }}
+      >
+        <Tab.Screen
+          name="Programmes"
+          component={Declaration}
+          options={{
+            tabBarLabel: 'Déclaration',
+            tabBarShowLabel: true,
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="format-list-bulleted" color={color} size={26} />
+            ),
+          }}
+        />
+
+        <Tab.Screen
+          name="Accueil"
+          component={Home}
+          options={{
+            tabBarLabel: 'Accueil',
+            tabBarShowLabel: true,
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="home" color={color} size={26} />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    );
+  }
+
+  const MyStack = () => {
+    return (
+      <Stack.Navigator initialRouteName='Login' screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Tabs" component={MyTabs} />
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Register" component={Register} />
+      </Stack.Navigator>
+    );
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <MyStack />
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ screens/Steppers.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Steppers = () => {
+  return (
+    <View>
+      <Text>Steppers</Text>
+    </View>
+  )
+}
+
+export default Steppers
+
+const styles = StyleSheet.create({})
