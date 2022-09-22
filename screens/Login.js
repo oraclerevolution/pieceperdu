13312a395d7e76b19dff9b4a7f9bcd4be89cb084
oracle2_dev <mac.n.drix@gmail.com>
2022-09-22 00:35:33 +0000

    creation des pages de steppers et de login

--- screens/Login.js
+++ screens/Login.js
@@ -1,14 +1,81 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Image, Text, View } from 'react-native'
+import React,{useState} from 'react'
+import { Button, TextInput } from 'react-native-paper'
+import * as Font from "expo-font"
 
-const Login = () => {
+const Login = ({navigation}) => {
+  const [email, setEmail] = useState(null)
+  const [password, setpassword] = useState(null)
+
+  const fontLoaded = async() => {
+    await Font.loadAsync({
+      // Load a font from a static resource
+      GothamBold: require('../assets/fonts/GothamBold.ttf'),
+      GothamLight: require('../assets/fonts/GothamLight.ttf'),
+      GothamBlack: require('../assets/fonts/GothamBlack.otf'),
+    });
+  }
+
   return (
-    <View>
-      <Text>Login</Text>
+    <View style={styles.container}>
+        <Text style={{marginBottom:"5%", fontSize:19}}>Hello Again !</Text>
+        <Text style={{fontFamily:"GothamBold", fontSize:16}}>Welcome back you've</Text>
+        <Text style={{fontFamily:"GothamBlack", marginBottom: "6%", fontSize:16}}>been missed !</Text>
+        <View style={styles.forms}>
+        <TextInput 
+            label="Entre votre email"
+            placeholder="assiajean@email.com"
+            value={email}
+            onChangeText={text => setEmail(text)}
+          />
+
+          <TextInput 
+            label="Entre votre mot de passe"
+            placeholder="***********"
+            value={password}
+            secureTextEntry
+            onChangeText={text => setpassword(text)}
+          />
+          <Text>Recovery Password</Text>
+          <Button mode='contained'>Sign In</Button>
+        </View>
+        <View style={{flexDirection:"row"}}>
+          <View style={{borderWidth:1, width:"30%", height:"0%", marginRight:"5%", position:"relative", top:"2.5%"}}></View>
+          <Text>Or continue with</Text>
+          <View style={{borderWidth:1, width:"30%", height:"0%", marginLeft:"5%", position:"relative", top:"2.5%"}}></View>
+        </View>
+        <View style={{flexDirection:"row"}}>
+            <View>
+              <Image source={require('../assets/google.png')} style={{width:40, height:40}} />
+            </View>
+            <View>
+              <Image source={require('../assets/facebbok.png')} style={{width:40, height:40}} />
+            </View>
+          </View>
     </View>
   )
 }
 
 export default Login
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    width:"100%",
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding:10
+  },
+  forms:{
+    borderWidth:1,
+    width:"100%",
+    padding:5
+  },
+  InputStyle:{
+    color:"black",
+    marginBottom:1,
+    marginLeft:5,
+    fontSize:14,
+    fontFamily:"GothamBlack"
+},
+})
--- screens/Steppers.js
+++ screens/Steppers.js
@@ -1,14 +1,48 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image } from 'react-native'
 import React from 'react'
+import {Button} from 'react-native-paper'
+import * as Font from 'expo-font';
+import { Constants } from 'expo-constants';
 
-const Steppers = () => {
+const Steppers = ({navigation}) => {
+
+  const fontLoaded = async() => {
+    await Font.loadAsync({
+      // Load a font from a static resource
+      GothamBold: require('../assets/fonts/GothamBold.ttf'),
+      GothamLight: require('../assets/fonts/GothamLight.ttf'),
+      GothamBlack: require('../assets/fonts/GothamBlack.otf'),
+    });
+  }
+
   return (
-    <View>
-      <Text>Steppers</Text>
+    <View style={styles.container}>
+      <View style={{flex:1, padding:5}}>
+        <Image
+          source={require('../assets/cover_live.jpeg')}
+          style={{width:"100%", height:"100%", borderRadius:20}}
+        />
+      </View>
+      <View style={{flex:1,justifyContent:"center", alignItems: 'center',padding:5}}>
+        <Text style={{textAlign:"center", fontFamily:"GothamBlack", fontSize:21}}>Retrouvez votre</Text>
+        <Text style={{textAlign:"center", fontFamily:"GothamBlack", fontSize:20, marginBottom:"5%"}}>Pièce perdu ici</Text>
+
+        <Text style={{textAlign:"center", fontFamily:"GothamLight", fontSize:16}}>Explore all the most exiting jobs roles based on your interest And study major</Text>
+
+        <View style={{flexDirection:"row"}}>
+          <Button mode='contained' labelStyle={{fontFamily:"GothamBlack"}} style={{margin:5, marginTop:"10%"}}>Register</Button>
+          <Button mode='outlined' labelStyle={{fontFamily:"GothamBlack"}} style={{margin:5, marginTop:"10%"}} onPress={()=>navigation.navigate('Login')}>Sign In</Button>
+        </View>
+      </View>
     </View>
   )
 }
 
 export default Steppers
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container:{
+    flex:1, 
+    padding:5,
+  }
+})
